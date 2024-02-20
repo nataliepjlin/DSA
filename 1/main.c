68cf9a13a5d611bf48e1496e372e64b2b214ed5b
nat <nataliepjlin@gmail.com>
2024-02-20 17:05:35 +0800

    hw0 p2

--- 1/main.c
+++ 1/main.c
@@ -1,66 +1,46 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
-#define N 21
-bool valid(int n, long long trial[]) {
-    if (n <= 2)
-        return true;
-    for (int i = 0; i < n - 2; i++) {
-        if (((trial[i + 1] - trial[i]) > 0 && (trial[i + 2] - trial[i + 1]) < 0) ||
-            ((trial[i + 1] - trial[i]) < 0 && (trial[i + 2] - trial[i + 1]) > 0))
-            continue;
-        return false;
-    }
-    return true;
+#define N 25
+int cmp(const void *a, const void *b){
+    long long *pa = (long long*)a, *pb = (long long*)b;
+    if(*pa > *pb) return 1;
+    else if(*pa < *pb) return -1;
+    return 0;
 }
-
-void perm(int idx, int n, bool used[], long long trial[N], const long long arr[], long long ans[N][N], int *cnt) {
-    if (!valid(idx, trial))
+void f(int idx, int n, bool used[], long long arr[], long long perm[], long long ans[N][N], int *cnt){
+    if(idx >= 3
+    && !((perm[idx - 2] - perm[idx - 3] > 0 && perm[idx - 1] - perm[idx - 2] < 0)
+    || (perm[idx - 2] - perm[idx - 3] < 0 && perm[idx - 1] - perm[idx - 2] > 0)))
         return;
-    if (idx == n) {
+    if(idx == n){
         for(int i = 0; i < n; i++)
-            ans[*cnt][i] = trial[i];
+            ans[*cnt][i] = perm[i];
         *cnt += 1;
         return;
     }
-    for (int i = 0; i < n; i++) {
-        if (i > 0 && arr[i] == arr[i - 1] && !used[i - 1])
-            continue;
-
-        if (!used[i]) {
-            used[i] = true;
-            trial[idx] = arr[i];
-            perm(idx + 1, n, used, trial, arr, ans, cnt);
-            trial[idx] = 0;
-            used[i] = false;
-        }
-    }
-}
-
-int cmp(const void *a, const void *b) {
-    long long *pa = (long long *)a, *pb = (long long *)b;
-    if (*pa > *pb)
-        return 1;
-    else if (*pa < *pb)
-        return -1;
-    return 0;
-}
-
-int main() {
-    int n;
-    scanf("%d", &n);
-    long long arr[N];
-    bool used[N];
-    for (int i = 0; i < n; i++) {
-        scanf("%lld", &arr[i]);
+    for(int i = 0; i < n; i++){
+        if((i && arr[i] == arr[i - 1] && !used[i - 1])
+        || used[i]) continue;
+        used[i] = true;
+        perm[idx] = arr[i];
+        f(idx + 1, n, used, arr, perm, ans, cnt);
+        perm[idx] = 0;
         used[i] = false;
     }
-    qsort(arr, n, sizeof(long long), cmp);
-
-    int cnt = 0;
-    long long trial[N];
+}
+int main(){
+    int n;
+    scanf("%d", &n);
     long long ans[N][N];
-    perm(0, n, used, trial, arr, ans, &cnt);
+    long long arr[N], perm[N];
+    bool used[N] = {0};
+    for(int i = 0; i < n; i++){
+        scanf("%lld", &arr[i]);
+    }
+    qsort(arr, n, sizeof(long long), cmp);
+    int cnt = 0;
+    f(0, n, used, arr, perm, ans, &cnt);
     printf("%d\n", cnt);
     for(int i = 0; i < cnt; i++){
         for(int j = 0; j < n; j++)
