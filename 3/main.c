7bdb8326872c1c05c62366cc6ee5f2cdf537155e
nat <nataliepjlin@gmail.com>
2024-02-22 14:56:13 +0800

    arr global

--- 3/main.c
+++ 3/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-int getMinIdx(int l, int r, int arr[]){
+int arr[100001];
+int getMinIdx(int l, int r){
     int m = l;
     for(int i = l + 1; i <= r; i++){
         if(arr[m] > arr[i])
@@ -12,10 +13,10 @@ void swap(int *a, int *b){
     *a = *b;
     *b = t;
 }
-int sort(int arr[], const int n, const int b){
+int sort(const int n, const int b){
     int used = 0;
     for(int i = 0; i < n; i++){
-        int m = getMinIdx(i, n - 1, arr);
+        int m = getMinIdx(i, n - 1);
         if(i != m){
             swap(&arr[i], &arr[m]);
             used++;
@@ -24,7 +25,7 @@ int sort(int arr[], const int n, const int b){
     }
     return used;
 }
-void print(int arr[], int n){
+void print(int n){
     for(int i = 0; i < n; i++){
         printf("%d ", arr[i]);
     }
@@ -32,12 +33,11 @@ void print(int arr[], int n){
 int main(){
     int n, b;
     scanf("%d%d", &n, &b);
-    int arr[n];
     for(int i = 0; i < n; i++)
         scanf("%d", &arr[i]);
-    int used = sort(arr, n, b);
+    int used = sort(n, b);
     printf("The array is ");
-    print(arr, n);
+    print(n);
     printf("after %d swaps.\n", used);
     return 0;
 }
