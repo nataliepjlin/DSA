5b16b84a56a0c49db4b9407b83c1d43e663c8815
nat <nataliepjlin@gmail.com>
2024-03-05 17:06:05 +0800

    int to void

--- 7/main.c
+++ 7/main.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
-int bs(int a[], int l, int r, const int key){
+void bs(int a[], int l, int r, const int key){
     printf("Searching %d in range [%d, %d].\n", key, l + 1, r + 1);
-    if(l <= r){
-        int m = (l + r) >> 1;
-        if(a[m] == key){
-            printf("Found at index %d.\n", m + 1);
-            return 1;
-        }
-        else if(a[m] < key) return bs(a, m + 1, r, key);
-        else return bs(a, l, m - 1, key);
+    if(l > r){
+        printf("Not found.\n");
+        return;
+    } 
+    int m = (l + r) >> 1;
+    if(a[m] == key){
+        printf("Found at index %d.\n", m + 1);
+        return;
     }
-    return 0;
+    else if(a[m] < key) bs(a, m + 1, r, key);
+    else bs(a, l, m - 1, key);
 }
 int main(){
     int n, key;
@@ -20,6 +21,6 @@ int main(){
         scanf("%d", &a[i]);
     }
 
-    if(!bs(a, 0, n - 1, key))
-        printf("Not found.\n");
+    bs(a, 0, n - 1, key);
+    return 0;
 }
