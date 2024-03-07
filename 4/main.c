a13edaebcc08a99cb85c3ab986941afc9583138a
nat <nataliepjlin@gmail.com>
2024-03-07 20:01:36 +0800

    cant del

--- 4/main.c
+++ 4/main.c
@@ -1,161 +1,119 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <limits.h>
-#include <time.h>
-
-#define MAX_LEVEL 20
-
-typedef struct Node {
-    int value;
-    struct Node** forward;
-} Node;
-
-typedef struct SkipList {
+#define MAXLAYER 20
+typedef struct Node{
+    struct Node **forward;
+    int key;
+}Node;
+typedef struct skipList{
     int level;
     Node* header;
-} SkipList;
-
-Node* createNode(int value, int level) {
-    Node* newNode = (Node*)malloc(sizeof(Node));
-    newNode->value = value;
-    newNode->forward = (Node**)malloc((level + 1) * sizeof(Node*));
-    return newNode;
-}
-
-SkipList* createSkipList() {
-    SkipList* skipList = (SkipList*)malloc(sizeof(SkipList));
-    skipList->level = 0;
-    skipList->header = createNode(INT_MAX, MAX_LEVEL);
-    for (int i = 0; i <= MAX_LEVEL; i++) {
-        skipList->header->forward[i] = NULL;
-    }
-    return skipList;
-}
-
+}skipList;
 int coinFlip(int layer, int key) {
-    if(layer == 0) return 1;
-    return (key / (1 << (layer - 1))) & 1;
+    return (layer == 0 || (key / (1 << (layer - 1))) & 1);
 }
-void insert(SkipList* skipList, int value) {
-    Node* previous[MAX_LEVEL + 1];
-    Node* current = skipList->header;
-
-    for (int i = skipList->level; i >= 0; i--) {
-        while (current->forward[i] != NULL && current->forward[i]->value > value) {
-            current = current->forward[i];
-        }
-        previous[i] = current;
+void initList(skipList *list){
+    Node *header = (Node *) malloc(sizeof(Node));
+    list->header = header;
+    header->key = INT_MAX;
+    header->forward = (Node **) malloc(sizeof(Node*) * (MAXLAYER + 1));
+    for (int i = 0; i < MAXLAYER; i++) {
+        header->forward[i] = NULL;
     }
-
-
-    if (coinFlip(skipList->level, value) && coinFlip(skipList->level + 1, value)) {
-        previous[skipList->level + 1] = skipList->header;
-        skipList->level += 1;
-    }
-
-    Node* newNode = createNode(value, skipList->level);
-
-    for (int i = 0; i <= skipList->level; i++) {
-        if(!coinFlip(i, value)) continue;
-        newNode->forward[i] = previous[i]->forward[i];
-        previous[i]->forward[i] = newNode;
-    }
+ 
+    list->level = 0;
 }
-void del(SkipList* skipList, int value) {
-    Node* previous[MAX_LEVEL + 1];
-    Node* current = skipList->header;
-
-    for (int i = skipList->level; i >= 0; i--) {
-        while (current->forward[i] != NULL && current->forward[i]->value > value) {
-            current = current->forward[i];
-        }
-        previous[i] = current;
-    }
-
-    current = current->forward[0];
-
-    if (current != NULL && current->value == value) {
-        for (int i = 0; i <= skipList->level; i++) {
-            if (previous[i]->forward[i] != current)
-                break;
-            previous[i]->forward[i] = current->forward[i];
-        }
-
-        free(current->forward);
-        free(current);
-        
-        while (skipList->level > 0 && skipList->header->forward[skipList->level] == NULL) {
-            skipList->level--;
-        }
-    }
-}
-void fastGet(SkipList* skipList, int value) {
-    Node* current = skipList->header;
+void fastGet(skipList *list, int key){
+    Node *node = list->header;
     int flag = 1;
-    for (int i = skipList->level; i >= 0; i--) {
-        while (current->forward[i] != NULL && current->forward[i]->value > value) {
-            printf("%d ", current->forward[i]->value);
-            current = current->forward[i];
+    for(int i = list->level; i >= 0; i--){
+        if(node->key != INT_MAX) printf("%d ", node->key);
+        while(node->forward[i] && key <= node->forward[i]->key){
+            printf("%d ", node->forward[i]->key);
+            node = node->forward[i];
             flag = 0;
         }
     }
-    current = current->forward[0];
-    if(current && current->value == value) printf("%d ", value);
     if(flag) printf("-1");
     printf("\n");
 }
-
-void slowGet(SkipList* skipList, int value) {
-    Node* current = skipList->header;
-    if(current == NULL || current->forward[0] == NULL
-    || current->forward[0]->value < value){
-        printf("-1\n");
-        return;
+void slowGet(skipList *list, int key){
+    Node *node = list->header;
+    int flag = 1;
+    while(node->forward[0] && key <= node->forward[0]->key){
+        printf("%d ", node->forward[0]->key);
+        node = node->forward[0];
+        flag = 0;
     }
-    while (current->forward[0] != NULL && current->forward[0]->value > value) {
-        printf("%d ", current->forward[0]->value);
-        current = current->forward[0];
-    }
-    current = current->forward[0];
-    if(current && current->value == value) printf("%d ", value);
+    if(flag) printf("-1");
     printf("\n");
 }
-
-void printSkipList(SkipList* skipList) {
-    printf("Skip List:\n");
-    for (int i = skipList->level; i >= 0; i--) {
-        Node* current = skipList->header->forward[i];
-        printf("Level %d: ", i);
-        while (current != NULL) {
-            printf("%d ", current->value);
-            current = current->forward[i];
+void printList(skipList *list){
+    for(int i = list->level; i >= 0; i--){
+        Node *node = list->header;
+        printf("Layer %d: ", i);
+        while(node->forward[i]){
+            printf("%d ", node->forward[i]->key);
+            node = node->forward[i];
         }
         printf("\n");
     }
-    printf("\n");
+    printf("-----\n");
 }
-
-int main() {
-    SkipList* skipList = createSkipList();
-
+void insert(skipList *list, int key){
+    Node *node = list->header;
+    Node *previous[MAXLAYER + 1] = {list->header};
+    for(int i = list->level; i >= 0; i--){
+        while(node->forward[i] && node->forward[i]->key > key)
+            node = node->forward[i];
+        previous[i] = node;
+    }
+    node = (Node*)malloc(sizeof(Node));
+    node->key = key;
+    node->forward = (Node**)malloc(sizeof(Node*) * (MAXLAYER));
+    for(int i = 0; i <= list->level && coinFlip(i, key); i++){
+        if(i == list->level && coinFlip(i + 1, key)){
+            previous[list->level + 1] = list->header;
+            list->level += 1;
+        }
+        node->forward[i] = previous[i]->forward[i];
+        previous[i]->forward[i] = node;
+    }
+}
+void del(skipList *list, int key){
+    Node *node = list->header;
+    Node *previous[MAXLAYER + 1] = {0};
+    for(int i = list->level; i >= 0; i--){
+        while(node->forward[i] && node->forward[i]->key > key)
+            node = node->forward[i];
+        previous[i] = node;
+    }
+    node = node->forward[0];
+    if(node && node->key == key){
+        for(int i = 0; i <= list->level; i++){
+            if(previous[i]->forward[i] != node)
+                break;
+            previous[i]->forward[i] = node->forward[i];
+        }
+        free(node);
+        if(list->level > 0 && list->header->forward[list->level] == NULL)
+            list->level -= 1;
+    }
+}
+int main(){
+    skipList list;
+    initList(&list);
     int m, t, k;
     scanf("%d", &m);
     for(int i = 0; i < m; i++){
         scanf("%d%d", &t, &k);
-        if(t == 1) slowGet(skipList, k);
-        else if(t == 2) fastGet(skipList, k);
-        else if(t == 3) insert(skipList, k);
-        else del(skipList, k);
+        if(t == 1) slowGet(&list, k);
+        else if(t == 2) fastGet(&list, k);
+        else if(t == 3) insert(&list, k);
+        else del(&list, k);
         #ifdef debug
-        printSkipList(skipList);
+        printList(&list);
         #endif
     }
-    return 0;
-
-    // Clean up
-    free(skipList->header);
-    free(skipList);
-
-    return 0;
 }
