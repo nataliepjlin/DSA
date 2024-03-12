08b3c160cd8dcfacf4ee9e3731d9061582b140f8
nat <nataliepjlin@gmail.com>
2024-03-13 00:01:09 +0800

    vec

--- 5/main.c
+++ 5/main.c
@@ -1,119 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
-typedef struct Node{
-    long long preSum;
-    struct Node *next, *prev;
-}Node;
-typedef struct Info{
-    Node *head;
-    Node *left, *right;
-    int cnt;//attack
-}Info;
-typedef struct BigInfo{
-    Info **info;
-}BigInfo;
-Node *genNode(long long data){
-    Node *node = (Node*)malloc(sizeof(Node));
-    node->preSum = data, node->next = NULL, node->prev = NULL;
-    return node;
+typedef struct vector_t{
+    long long *presums;
+    int cap;
+    int size;
+}vector_t;
+vector_t *vector_new(){
+    return (vector_t*)malloc(sizeof(vector_t));
 }
-void initInfo(BigInfo *list, const int n){
-    list->info = (Info**)malloc(sizeof(Info*) * n);
-    for(int i = 0; i < n; i++){
-        list->info[i] = (Info*)malloc(sizeof(Info));
-        list->info[i]->head = genNode(0ll);
-        list->info[i]->left = list->info[i]->head;
-        list->info[i]->right = list->info[i]->head;
-        list->info[i]->cnt = 0;
+vector_t **bigVec_new(const int n){
+    return (vector_t**)malloc(sizeof(vector_t*) * n);
+}
+void vector_init(vector_t *obj){
+    obj->presums = calloc(2, sizeof(long long));
+    obj->cap = 1;
+    obj->size = 0;
+}
+void vector_destroy(vector_t *obj){
+    free(obj->presums);
+    free(obj);
+}
+void push_back(vector_t *obj, const long long sum){
+    #ifdef debug
+    printf("sum = %lld\n", sum);
+    #endif
+    if(obj->size != 0){
+        obj->cap += 1;//cap = 1
+        obj->presums = realloc(obj->presums, (obj->cap + 1) * sizeof(long long));//real cap = 2(cap + 1)
+        #ifdef debug
+        printf("obj->size = %d, realloc %d\n", obj->size, obj->cap);
+        #endif
     }
-}
-void extendInfo(Info *info, long long powDiff, const int m){
-    Node *newNode = genNode(info->right->preSum + powDiff);
-    info->right->next = newNode;
-    newNode->prev = info->right;
-    info->right = info->right->next;
-    if(info->cnt >= m) info->left = info->left->next;
-    info->cnt += 1;
-}
-int min(int a, int b){
-    return a < b ? a : b;
+    assert(obj->presums != NULL);
+    obj->presums[obj->size + 1] = sum + obj->presums[obj->size];
+    obj->size += 1;
 }
 int main(){
     int n, t, m;
     scanf("%d%d%d", &n, &t, &m);
-    BigInfo list;//info with player's id
-    initInfo(&list, n);
+    vector_t **bigVec = bigVec_new(n);
     long long powers[n];//power of ith ranking player
     int rank[n];//use id to find rank
     int ids[n];//use rank to find id
     for(int i = 0; i < n; i++){
         scanf("%lld", &powers[i]);
         rank[i] = i, ids[i] = i;
+        bigVec[i] = vector_new();
+        vector_init(bigVec[i]);
     }
     int op, a, b;
     long long op2 = 0, q;
     for(int i = 0; i < t; i++){
         scanf("%d", &op);
-        if(op == 4){
-            scanf("%d%d", &a, &b);
-            a--;
-            if(b == m || b >= list.info[a]->cnt){
-                printf("%lld\n", list.info[a]->right->preSum - list.info[a]->left->preSum);
-            }
-            else{
-                int step = min(m, list.info[a]->cnt) - b;
-                #ifdef debug
-                printf("step = %d\n", step);
-                #endif
-                if(step > (m >> 1)){
-                    #ifdef debug
-                    printf("starting from right will be faster\n");
-                    #endif
-                    step -= (m >> 1);
-                    int tenStep = step / 10;
-                    Node *tmpLeft = list.info[a]->right;
-                    for(int i = 0; i < tenStep; i++)
-                        tmpLeft = tmpLeft->prev->prev->prev->prev->prev->prev->prev->prev->prev->prev;
-                    step %= 10;
-                    int twoStep = step >> 1;
-                    for(int i = 0; i < twoStep; i++)
-                        tmpLeft = tmpLeft->prev->prev;
-                    if(step & 1) tmpLeft = tmpLeft->prev;
-                    printf("%lld\n", list.info[a]->right->preSum - tmpLeft->preSum);
-                }
-                else{
-                    #ifdef debug
-                    printf("starting from left will be faster\n");
-                    #endif
-                    int tenStep = step / 10;
-                    Node *tmpLeft = list.info[a]->left;
-                    for(int i = 0; i < tenStep; i++)
-                        tmpLeft = tmpLeft->next->next->next->next->next->next->next->next->next->next;
-                    step %= 10;
-                    int twoStep = step >> 1;
-                    for(int i = 0; i < twoStep; i++)
-                        tmpLeft = tmpLeft->next->next;
-                    if(step & 1) tmpLeft = tmpLeft->next;
-                    printf("%lld\n", list.info[a]->right->preSum - tmpLeft->preSum);
-                }
-            }
-        }
-        else if(op == 1){
-            scanf("%d", &a);
-            a--;
-            int rankA = rank[a];
-            if(rankA != 0){
-                int prevID = ids[rankA - 1];
-                long long powDiff = (powers[rankA - 1] - powers[rankA]) + op2;
-                powers[rankA] += powDiff;
-                extendInfo(list.info[a], powDiff, m);
-                rank[a]--, rank[prevID]++;
-                ids[rankA - 1] = a, ids[rankA] = prevID;
-            }
-        }
-        else if(op == 2) op2++;
-        else{//op == 3
+        if(op == 2) op2++;
+        else if(op == 3){
             scanf("%lld", &q);
             if(q > powers[0] + op2 * (n - 1)){
                 printf("0 0\n");
@@ -130,6 +71,32 @@ int main(){
             }
             printf("%d %d\n", rnk + 1, ids[rnk] + 1);
         }
+        else if(op == 1){
+            scanf("%d", &a);
+            a--;
+            int rankA = rank[a];
+            if(rankA != 0){
+                int prevID = ids[rankA - 1];
+                #ifdef debug
+                printf("swap player %d and %d's ranking\n", prevID, a);
+                #endif
+                long long powDiff = (powers[rankA - 1] - powers[rankA]) + op2;
+                powers[rankA] += powDiff;
+                push_back(bigVec[a], powDiff);
+                #ifdef debug
+                printf("player %d gain pow %lld\n", a, powDiff);
+                #endif
+                rank[a]--, rank[prevID]++;
+                ids[rankA - 1] = a, ids[rankA] = prevID;
+            }
+        }
+        else{//op = 4;
+            scanf("%d%d", &a, &b);
+            a--;
+            int idx = bigVec[a]->size;
+            if(b >= idx) printf("%lld\n", bigVec[a]->presums[idx]);
+            else printf("%lld\n", bigVec[a]->presums[idx] - bigVec[a]->presums[idx - b]);
+        }
         #ifdef debug
         printf("\n%dth task:\n", i);
         for(int j = 0; j < n; j++){
@@ -139,16 +106,13 @@ int main(){
         #endif
     }
     printf("\n");
-    for(int i = 0; i < n; i++){//second part
-        if(!list.info[i]->cnt){
-            printf("0\n");
-            continue;
-        }
-        printf("%d", list.info[i]->cnt);
-        while(list.info[i]->head->next){
-            printf(" %lld", list.info[i]->head->next->preSum - list.info[i]->head->preSum);
-            list.info[i]->head = list.info[i]->head->next;
-        }
+    for(int i = 0; i < n; i++){
+        printf("%d", bigVec[i]->size);
+        for(int j = 1; j <= bigVec[i]->size; j++)
+            printf(" %lld", bigVec[i]->presums[j] - bigVec[i]->presums[j - 1]);
         printf("\n");
+        vector_destroy(bigVec[i]);
     }
-}
+    free(bigVec);
+    return 0;
+}   
