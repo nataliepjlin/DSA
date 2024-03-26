1ab7a91471a1d2472d615d15ecd92e44a259c6db
nat <nataliepjlin@gmail.com>
2024-03-26 20:24:27 +0800

    init

---
+++ 10/main.c
@@ -0,0 +1,162 @@
+#include <stdio.h>
+#include <stdlib.h>
+enum OP{ADD = 1, PRINT, MOVE};
+typedef struct node_t{
+    int id, deg;
+    int priorty;//value in most ref
+    struct node_t *child, *parent, *sib;
+}node_t;
+typedef struct heap_t{
+    node_t *head;
+    int numOfJob;
+}heap_t;
+node_t *genNode(int id, int priorty){
+    node_t *node = malloc(sizeof(node_t));
+    node->id = id, node->priorty = priorty;
+    node->deg = 0;
+    node->child = node->parent = node->sib = NULL;
+    return node;
+}
+heap_t *genHeap(){
+    heap_t *heap = (heap_t*)malloc(sizeof(heap_t));
+    heap->head = NULL;
+    heap->numOfJob = 0;
+    return heap;
+}
+node_t *heapMerge(heap_t *h1, heap_t *h2){
+    if(h1->head == NULL) return h2->head;
+    if(h2->head == NULL) return h1->head;
+    node_t *head, *cur1 = h1->head, *cur2 = h2->head, *tail;
+    if(h1->head->deg >= h2->head->deg){
+        head = h1->head;
+        cur1 = cur1->sib;
+    }
+    else{
+        head = h2->head;
+        cur2 = cur2->sib;
+    }
+    tail = head;
+    while(cur1 && cur2){
+        if(cur1->deg >= cur2->deg){
+            tail->sib = cur1;
+            cur1 = cur1->sib;
+        }
+        else{
+            tail->sib = cur2;
+            cur2 = cur2->sib;
+        }
+        tail = tail->sib;
+    }
+    tail->sib = (cur1) ? cur1 : cur2;
+    return head;
+}
+node_t *heapUnion(heap_t *origin, heap_t *uni){
+    node_t *head = heapMerge(origin, uni);
+    origin->head = NULL, uni->head = NULL;
+    if(head == NULL) return NULL;
+    node_t *prev = NULL, *cur = head, *next = head->sib;
+    while(next){
+        if(cur->deg != next->deg || (next->sib && next->sib->deg == cur->deg)){
+            prev = cur;
+            cur = next;
+        }
+        else{
+            if(cur->priorty >= next->priorty){
+                cur->sib = next->sib;
+                next->parent = cur;
+                next->sib = cur->child;
+                cur->child = next;
+                cur->deg += 1;
+            }
+            else{
+                if(prev == NULL) head = next;
+                else prev->sib = next;
+                cur->parent = next;
+                cur->sib = next->child;
+                next->child = cur;
+                next->deg += 1;
+                cur = next;
+            }
+        }
+        next = next->sib;
+    }
+    return head;
+}
+void heapInsert(heap_t *h, int id, int priorty){
+    node_t *node = genNode(id, priorty);
+    heap_t *tmp = genHeap();
+    tmp->head = node;
+    h->head = heapUnion(h, tmp);
+    h->numOfJob += 1;
+    free(tmp);
+}
+void heapRemove(heap_t *h, node_t *node, node_t *prev){
+    if(node == h->head) h->head = node->sib;
+    else prev->sib = node->sib;
+    node_t *newHead = NULL, *child = node->child;
+    while(child){
+        node_t *next = child->sib;
+        child->sib = newHead;
+        child->parent = NULL;
+        newHead = child;
+        child = next;
+    }
+    heap_t *tmp = genHeap();
+    tmp->head = newHead;
+    h->head = heapUnion(h, tmp);
+    h->numOfJob -= 1;
+    free(tmp);
+}
+node_t *heapMin(heap_t *h){
+    if(h->head == NULL) return NULL;
+    node_t *max = h->head;
+    node_t *max_prev = NULL, *next = max->sib, *next_prev = max;
+    while(next){
+        if(next->priorty > max->priorty){
+            max = next;
+            max_prev = next_prev;
+        }
+        next_prev = next;
+        next = next->sib;
+    }
+    heapRemove(h, max, max_prev);
+    return max;
+}
+int main(){
+    int n, m, op, id, priority, pid1, pid2;
+    scanf("%d%d", &n, &m);
+    heap_t **heapArr = calloc(n, sizeof(heap_t*));
+    for(int i = 0; i < m; i++){
+        #ifdef debug
+        printf("\n");
+        #endif
+        scanf("%d", &op);
+        if(op == ADD){
+            scanf("%d%d%d", &id, &priority, &pid1);
+            pid1--;
+            #ifdef debug
+            printf("add (id, priorty) = (%d, %d) to %dth printer\n", id, priority, pid1);
+            #endif
+            if(heapArr[pid1] == NULL) heapArr[pid1] = genHeap();
+            heapInsert(heapArr[pid1], id, priority);
+            printf("%d jobs waiting on printer %d\n", heapArr[pid1]->numOfJob, pid1 + 1);
+        }
+        else if(op == PRINT){
+            scanf("%d", &pid1);
+            pid1--;
+            node_t *mx = heapMin(heapArr[pid1]);
+            if(mx == NULL) printf("no documents in queue\n");
+            else printf("%d printed\n", mx->id);
+            free(mx);
+        }
+        else{
+            scanf("%d%d", &pid1, &pid2);
+            pid1--, pid2--;
+            heapArr[pid2]->head = heapUnion(heapArr[pid1], heapArr[pid2]);
+            heapArr[pid2]->numOfJob += heapArr[pid1]->numOfJob;
+            heapArr[pid1]->numOfJob = 0;
+            printf("%d jobs waiting on printer %d after moving\n", heapArr[pid2]->numOfJob, pid2 + 1);
+        }
+    }
+    free(heapArr);
+}
