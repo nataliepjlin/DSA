f18de57f79b29ce8076d87f84ed884592df0acc9
nat <nataliepjlin@gmail.com>
2024-03-26 17:09:19 +0800

    1e5 to 1e6

--- 12/main.c
+++ 12/main.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <limits.h>
-int arr[100005];
+
+int arr[1000005];
+
 #define PARENT(i) (i / 2)
+
 void swap(int *a, int *b){
     int t = *a;
     *a = *b, *b = t;
 }
+
 void heapIncreaseKey(int i, int key){
     arr[i] = key;
     while(i > 1 && arr[PARENT(i)] < arr[i]){
@@ -13,12 +17,13 @@ void heapIncreaseKey(int i, int key){
         i = PARENT(i);
     }
 }
+
 int main(){
     int n, key;
     scanf("%d", &n);
-    for(int i = 0; i < n; i++){
+    for(int i = 1; i <= n; i++){
         scanf("%d", &key);
-        heapIncreaseKey(i + 1, key);
+        heapIncreaseKey(i, key);
     }
     for(int i = 1; i <= n; i++)
         printf("%d%c", arr[i], " \n"[i == n]);
