9e306d5e31de7b2d098c3c46e776c5ce03e26beb
nat <nataliepjlin@gmail.com>
2024-03-30 13:06:58 +0800

    op3 binary lifting

--- 11/main.c
+++ 11/main.c
@@ -1,117 +1,109 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define N 1000001
-enum OP{DOWN = 1, UP, PLAN, GUESS, DISCOVER};
-typedef struct path_t path_t;
-typedef struct dungent_t dungent_t;
-struct path_t{
-    int len, u, v;
-    path_t *prev_down, *next_down;
-};
-struct dungent_t{
-    long long treasure;
-    path_t *down_h, *down_cur, *up;
-};
-dungent_t *init_dungent(){
-    dungent_t *d = malloc(sizeof(dungent_t));
-    d->treasure = -1;
-    d->down_h = d->down_cur = NULL;
-    d->up = NULL;
+typedef struct down_t{
+    int v;
+    struct down_t *next;
+}down_t;
+typedef struct big_down{
+    down_t *down_h, *down_cur;
+}big_down;
+typedef struct info_t{
+    int u;
+    long long len;//len with u
+}info_t;
+down_t *gen_down(int v){
+    down_t *d = malloc(sizeof(down_t));
+    d->v = v, d->next = NULL;
     return d;
 }
-path_t *init_path(int u, int v, int len){
-    path_t *p = malloc(sizeof(path_t));
-    p->len = len, p->u = u, p->v = v;
-    p->prev_down = NULL;
-    p->next_down = NULL;
-    return p;
+void extend_down(big_down *d, int v){
+    down_t *dwn = gen_down(v);
+    if(d->down_h == NULL) d->down_h = dwn;
+    else d->down_cur->next = dwn;
+    d->down_cur = dwn;
 }
-void print_path(path_t *p){
-    while(p){
-        printf("(%d, %d, %d)->", p->u, p->v, p->len);
-        p = p->next_down;
+void pop_down(big_down *d){
+    d->down_h = d->down_h->next;
+    if(d->down_h == NULL) d->down_cur = NULL;
+}
+void print_downs(int u, big_down *bd){
+    down_t *cur = bd->down_h;
+    while(cur){
+        printf("(%d, %d)->", u, cur->v);
+        cur = cur->next;
     }
     printf("X\n");
 }
 int main(){
-    int n, m, q, u, v, len, cur = 0, op;
-    long long ti;
+    int n, m, q, u, v, LOG = 0, op;
+    long long len;
     scanf("%d%d%d", &n, &m, &q);
-    dungent_t *dunArr[n];
-    for(int i = 0; i < n; i++) dunArr[i] = init_dungent();
-    for(int i = 0; i < m; i++){//create vector here???
-        scanf("%d%d%d", &u, &v, &len);
-        path_t *p = init_path(u, v, len);
-        if(dunArr[u]->down_h == NULL){
-            dunArr[u]->down_h = p;
-            dunArr[u]->down_cur = dunArr[u]->down_h;
-        }
-        else{
-            dunArr[u]->down_cur->next_down = p;
-            p->prev_down = dunArr[u]->down_cur;
-            dunArr[u]->down_cur = dunArr[u]->down_cur->next_down;
-        }
-        dunArr[v]->up = p;//use same path node, may cause RE???
+    info_t **vec = malloc(sizeof(info_t*) * n);
+    big_down downs[n];
+    while((1 << LOG) <= n) LOG++;
+    LOG++;
+    for(int i = 0; i < n; i++){
+        vec[i] = malloc(LOG * sizeof(info_t));
+        downs[i].down_h = downs[i].down_cur = NULL;
+    }
+    for(int i = 0; i < m; i++){
+        scanf("%d%d%lld", &u, &v, &len);
+        vec[v][0].u = u;//vth dun's 0th(direct) ancestor
+        vec[v][0].len = len;
+        extend_down(&downs[u], v);
         #ifdef debug
-        printf("down_h: ");
-        print_path(dunArr[u]->down_h);
-        printf("\nup: ");
-        print_path(dunArr[v]->up);
+        print_downs(u, &downs[u]);
+        #endif
+    }//set direct ancestor and descendants
+    vec[0][0].u = 0, vec[0][0].len = 0;
+    for(int i = 0; i < n; i++){
+        #ifdef debug
+        printf("%dth dun 1st ancestor = %d, len = %lld\n", i, vec[i][0].u, vec[i][0].len);
+        #endif
+        for(int j = 1; j < LOG; j++){
+            vec[i][j].u = vec[ vec[i][j - 1].u ][j - 1].u;
+            vec[i][j].len = vec[ vec[i][j - 1].u ][j - 1].len + vec[i][j - 1].len;
+            #ifdef debug
+            printf("%dth dun 2^%dth ancestor = %d, len = %lld\n", i, j, vec[i][j].u, vec[i][j].len);
+            #endif
+        }
+        #ifdef debug
         printf("\n");
         #endif
-    }
+    }//binary lifting
+
+    int cur = 0;
+    long long ti;
     for(int i = 0; i < q; i++){
         scanf("%d", &op);
-        #ifdef debug
-        printf("op = %d\n", op);
-        #endif
-        if(op == DOWN){
-            if(dunArr[cur]->down_h == NULL) printf("-1\n");
+        if(op == 1){
+            if(downs[cur].down_h == NULL) printf("-1\n");
             else{
-                cur = dunArr[cur]->down_h->v;
+                cur = downs[cur].down_h->v;
                 printf("%d\n", cur);
             }
         }
-        else if(op == UP){
+        else if(op == 2){
             if(cur == 0) printf("-1\n");
             else{
-                v = cur, u =  dunArr[v]->up->u;
-                #ifdef debug
-                printf("u = %d, v = %d\n", u, v);
-                #endif
-                path_t *p = dunArr[v]->up;
-                dunArr[v]->up = NULL;
-                if(p->prev_down == NULL){//p == dunArr[u]->down_h
-                    dunArr[u]->down_h = (dunArr[u]->down_h)->next_down;
-                    if(dunArr[u]->down_h)
-                        dunArr[u]->down_h->prev_down = NULL;
-                }
-                else{
-                    (p->prev_down)->next_down = p->next_down;
-                    if(p->next_down) (p->next_down)->prev_down = p->prev_down;
-                }
-                free(p);
-                cur = u;
+                cur = vec[cur][0].u;
+                pop_down(&downs[cur]);
                 printf("%d\n", cur);
-                #ifdef debug
-                printf("down_h: ");
-                print_path(dunArr[u]->down_h);
-                printf("\nup: ");
-                print_path(dunArr[v]->up);
-                printf("\n");
-                #endif
             }
         }
-        else if(op == PLAN){
+        else if(op == 3){
             scanf("%lld", &ti);
-            int tmp_cur = cur;
-            long long newti;
-            while(tmp_cur 
-            && (newti = ti - dunArr[tmp_cur]->up->len) >= 0){
-                tmp_cur = dunArr[tmp_cur]->up->u;
-                ti = newti;
-            }//change to binary search+prefix sum????
-            printf("%d\n", tmp_cur);
+            int low = 0, high = LOG - 1, ans = cur;
+            while(low <= high){
+                int mid = (low + high) >> 1;
+                if(vec[cur][mid].len <= ti){
+                    ans = vec[cur][mid].u;
+                    low = mid + 1;
+                }
+                else high = mid - 1;
+            }
+            printf("%d\n", ans);
         }
         else break;
     }
