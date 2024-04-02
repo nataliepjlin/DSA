2d0b91986f715a907c5ef75face0adc987e33eba
nat <nataliepjlin@gmail.com>
2024-04-02 22:48:05 +0800

    calloc info

--- 11/main.c
+++ 11/main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#define N 1000001
 typedef struct down_t{
     int v;
     struct down_t *next;
@@ -10,6 +9,7 @@ typedef struct info_t{
     down_t *down_h, *down_cur;
     bool has;
     long long treasure;
+    int cnt;
 }info_t;
 typedef struct up_t{
     int u;
@@ -20,25 +20,17 @@ down_t *gen_down(int v){
     d->v = v, d->next = NULL;
     return d;
 }
-void extend_down(info_t *d, int v){
+void extend_down(info_t *info, int v){
     down_t *dwn = gen_down(v);
-    if(d->down_h == NULL) d->down_h = dwn;
-    else d->down_cur->next = dwn;
-    d->down_cur = dwn;
+    if(info->down_h == NULL) info->down_h = dwn;
+    else info->down_cur->next = dwn;
+    info->cnt += 1;
+    info->down_cur = dwn;
 }
-void pop_down(info_t *d){
-    down_t *next = d->down_h->next;
-    free(d->down_h);
-    d->down_h = next;
-    if(d->down_h == NULL) d->down_cur = NULL;
-}
-void print_downs(int u, info_t *bd){
-    down_t *cur = bd->down_h;
-    while(cur){
-        printf("(%d, %d)->", u, cur->v);
-        cur = cur->next;
-    }
-    printf("X\n");
+void pop_down(info_t *info){
+    down_t *next = info->down_h->next;
+    free(info->down_h);
+    info->down_h = next;
 }
 int main(){
     int n, m, q, u, v, LOG = 0, op;
@@ -47,11 +39,7 @@ int main(){
     while((1 << LOG) <= n) LOG++;
     LOG++;
     up_t up[n][LOG];
-    info_t info[n];
-    for(int i = 0; i < n; i++){
-        info[i].down_h = info[i].down_cur = NULL;
-        info[i].has = false;
-    }
+    info_t *info = calloc(n,  sizeof(info_t));
     for(int i = 0; i < m; i++){
         scanf("%d%d%lld", &u, &v, &len);
         up[v][0].u = u;
@@ -89,50 +77,48 @@ int main(){
             scanf("%lld", &ti);
             int ans = cur;
             while(ans != 0 && ti >= up[ans][0].len){
-                int low = 0, high = LOG - 1, tmpcur = ans;
+                int low = 0, high = LOG - 1, idx = ans;
                 #ifdef debug
-                printf("tmpcur = %d\n", tmpcur);
+                printf("idx = %d\n", idx);
                 #endif
                 long long len = 0; 
                 while(low <= high){
                     int mid = (low + high) >> 1;
-                    if(up[tmpcur][mid].len <= ti){
-                        ans = up[tmpcur][mid].u;
-                        len = up[tmpcur][mid].len;
+                    if(up[idx][mid].len <= ti){
+                        ans = up[idx][mid].u;
+                        len = up[idx][mid].len;
                         low = mid + 1;
                     }
                     else high = mid - 1;
                 }
-                #ifdef debug
-                printf("len = %lld, ans = %d\n", len, ans);
-                #endif
                 ti -= len;
             }
             printf("%d\n", ans);
         }
         else if(op == 5){
             scanf("%lld", &pi);
-            int tmpcur = cur, neg = 0;
-            while(tmpcur != 0){
-                if(info[tmpcur].has == false){
-                    info[tmpcur].treasure = pi;
-                    info[tmpcur].has = true;
+            int idx = cur, neg = -1;
+            while(idx != 0){
+                if(info[idx].has == false){
+                    info[idx].treasure = pi;
+                    info[idx].has = true;
                     break;
                 }
-                long long old = info[tmpcur].treasure;
-                info[tmpcur].treasure = pi;
-                if(old < up[tmpcur][0].len){
-                    if(!neg) neg = (old < 0) ? tmpcur : up[tmpcur][0].u;
+                long long old = info[idx].treasure;
+                info[idx].treasure = pi;
+                if(old < up[idx][0].len){
+                    if(neg == -1) neg = (old < 0) ? idx : up[idx][0].u;
                     pi = -1;
                 }
-                else pi = old - up[tmpcur][0].len;
-                tmpcur = up[tmpcur][0].u;
+                else pi = old - up[idx][0].len;
+                idx = up[idx][0].u;
             }
-            if(tmpcur == 0){
+            if(idx == 0){
                 if(pi >= 0) printf("value remaining is %lld\n", pi);
                 else printf("value lost at %d\n", neg);
             }
         }
         else break;
     }
+    free(info);
 }
