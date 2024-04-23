36d2b55f49609afbeac4b58e0b377911445a582a
nat <nataliepjlin@gmail.com>
2024-04-23 21:43:40 +0800

    move print to DFS

--- 17/main.c
+++ 17/main.c
@@ -22,14 +22,6 @@ void insertEdge(Vertex *u, int v){
     else u->edgeTail->next = node;
     u->edgeTail = node;
 }
-void printEdge(Vertex *u){
-    ListNode *cur = u->edgeHead;
-    while(cur != NULL){
-        printf("%d->", cur->v + 1);
-        cur = cur->next;
-    }
-    printf("X\n");
-}
 void DFS_visit(Vertex *vertex, int *time, const int u){
     *time += 1;
     vertex[u].d = *time;
@@ -39,18 +31,12 @@ void DFS_visit(Vertex *vertex, int *time, const int u){
         if(vertex[node->v].color == 0){
             vertex[node->v].parent = u;
             node->pathType = 0;
-            #ifdef debug
-            printf("set edge (%d, %d) to type %d\n", u + 1, node->v + 1, node->pathType);
-            #endif
             DFS_visit(vertex, time, node->v);
         }
         else{
             if(vertex[node->v].color == -1) node->pathType = 1;
             else if(vertex[u].d < vertex[node->v].d) node->pathType = 2;
-            else if(vertex[u].d > vertex[node->v].d) node->pathType = 3;
-            #ifdef debug
-            printf("set edge (%d, %d) to type %d\n", u + 1, node->v + 1, node->pathType);
-            #endif
+            else node->pathType = 3;
         }
         node = node->next;
     }
@@ -60,8 +46,17 @@ void DFS_visit(Vertex *vertex, int *time, const int u){
 }
 void DFS(Vertex *vertex, const int v){
     int time = 0;
+    char dict[] = "TBFC";
     for(int i = 0; i < v; i++){
         if(vertex[i].color == 0) DFS_visit(vertex, &time, i);
+        
+        printf("%d ", i + 1);
+        ListNode *cur = vertex[i].edgeHead;
+        while(cur != NULL){
+            printf("%c", dict[cur->pathType]);
+            cur = cur->next;
+        }
+        printf("\n");
     }
 }
 int main(){
@@ -74,19 +69,7 @@ int main(){
             scanf("%d", &n);
             insertEdge(&vertex[i], n - 1);
         }
-        #ifdef debug
-        printEdge(&vertex[i]);
-        #endif
     }
     DFS(vertex, v);
-    char dict[] = "TBFC";
-    for(int i = 0; i < v; i++){
-        printf("%d ", i + 1);
-        ListNode *cur = vertex[i].edgeHead;
-        while(cur != NULL){
-            printf("%c", dict[cur->pathType]);
-            cur = cur->next;
-        }
-        printf("\n");
-    }
+    return 0;
 }
