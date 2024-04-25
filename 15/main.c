d110e31199c040ae77f7aa40b576303b4c875e50
nat <nataliepjlin@gmail.com>
2024-04-25 20:46:02 +0800

    test vectorize code

--- 15/main.c
+++ 15/main.c
@@ -1,126 +1,121 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#define B 100001
 enum COLOR{WHITE, GREY, BLACK};
-typedef struct ListNode{
-    int v;
-    struct ListNode *next;
-}ListNode;
 typedef struct Vertex{
     int color;
-    int d, f;
     int parent;
-    ListNode *edgeHead, *edgeTail;
+    int *idx;
+    int edge_size;
 }Vertex;
-typedef struct Time{
-    int idx, *f;
-}Time;
-ListNode *genNode(const int v){
-    ListNode *node = malloc(sizeof(ListNode));
-    node->v = v, node->next = NULL;
-    return node;
-}
+typedef struct RootNode{
+    int self_idx;
+    int *children, child_cnt;
+    int *colors, color_cnt;
+}RootNode;
+typedef struct RootNodes{
+    RootNode *roots;
+    int size;
+}RootNodes;
 void insertEdge(Vertex *u, const int v){
-    ListNode *node = genNode(v);
-    if(u->edgeHead == NULL) u->edgeHead = node;
-    else u->edgeTail->next = node;
-    u->edgeTail = node;
-}
-void DFS_visit(Vertex *vertex, int *time, const int u){
-    *time += 1;
-    vertex[u].d = *time;
-    vertex[u].color = GREY;
-    ListNode *node = vertex[u].edgeHead;
-    while(node != NULL){
-        if(vertex[node->v].color == WHITE){
-            vertex[node->v].parent = u;
-            DFS_visit(vertex, time, node->v);
-        }
-        node = node->next;
-    }
-    vertex[u].color = BLACK;
-    *time += 1;
-    vertex[u].f = *time;
-}
-void DFS(Vertex *vertex, const int b){
-    int time = 0;
-    for(int i = 0; i < b; i++){
-        if(vertex[i].color == WHITE) DFS_visit(vertex, &time, i);
-        #ifdef debug
-        printf("%dth vertex time = (%d, %d)\n", i, vertex[i].d, vertex[i].f);
-        #endif
-    }
-}
-int cmp(const void *a, const void *b){
-    Time *pa = (Time*)a, *pb = (Time*)b;
-    return *(pb->f) - *(pa->f);
+    u->edge_size += 1;
+    u->idx = realloc(u->idx, sizeof(int) * (u->edge_size));
+    u->idx[ u->edge_size - 1  ] = v;
 }
 void printEdge(Vertex *vertex, const int b){
     for(int i = 0; i < b; i++){
-        ListNode *cur = vertex[i].edgeHead;
-        printf("%dth vertex, parent = %d, edges: ", i, vertex[i].parent);
-        while(cur != NULL){
-            printf("%d->", cur->v);
-            cur = cur->next;
+        printf("%d:", i);
+        for(int j = 0; j < vertex[i].edge_size; j++){
+            printf(" %d", vertex[i].idx[j]);
         }
-        printf("X\n");
+        printf("\n");
     }
 }
-void printTime(Time *finishOrder, const int b){
-    for(int i = 0; i < b; i++)
-        printf("(%d, %d)%c", finishOrder[i].idx, *(finishOrder[i].f), " \n"[i == b - 1]);
+void DFS_visit(Vertex *vertex, const int u, int *finish_order, int *fi){
+   vertex[u].color = GREY;
+    for(int i = 0; i < vertex[u].edge_size; i++){
+        if(vertex[ vertex[u].idx[i] ].color == WHITE){
+            vertex[ vertex[u].idx[i] ].parent = u;
+            DFS_visit(vertex, vertex[u].idx[i], finish_order, fi);
+        }
+    }
+    vertex[u].color = BLACK;
+    finish_order[*fi] = u;
+    *fi += 1;
 }
-void getParent(Vertex *trans_v, int cur){
-    int root = cur;
-    for(; trans_v[root].parent >= 0; root = trans_v[root].parent);
-    while(cur != root){
-        int parent = trans_v[cur].parent;
-        trans_v[cur].parent = root;
-        cur = parent;
+RootNodes *genRootNodes(){
+    RootNodes *root_nodes = malloc(sizeof(RootNodes));
+    root_nodes->roots = NULL, root_nodes->size = 0;
+    return root_nodes;
+}
+void rnodes_push_back(RootNodes *rnodes, const int idx){
+    rnodes->size += 1;
+    rnodes->roots = realloc(rnodes->roots, rnodes->size * (sizeof(RootNode)));
+    RootNode rnode = rnodes->roots[ rnodes->size - 1 ];
+    rnode.child_cnt = 0, rnode.color_cnt = 0;
+    rnode.children = NULL, rnode.colors = NULL;
+    rnode.self_idx = idx;
+}
+void rnode_push_child(RootNode *rnode, const int idx){
+    rnode->child_cnt += 1;
+    rnode->children = realloc(rnode->children, rnode->child_cnt * (sizeof(int)));
+    rnode->children[ rnode->child_cnt - 1 ] = idx;
+}
+void dfs2(Vertex *vertex, const int u, RootNode *rnode){
+    vertex[u].color = GREY;
+    for(int i = 0; i < vertex[u].edge_size; i++){
+        if(vertex[ vertex[u].idx[i] ].color == WHITE){
+            rnode_push_child(rnode, vertex[u].idx[i]);
+            vertex[ vertex[u].idx[i] ].parent = u;
+            dfs2(vertex, vertex[u].idx[i], rnode);
+        }
+    }
+    vertex[u].color = BLACK;
+}
+void DFS(Vertex *vertex, const int b, int *finish_order){
+    int fi = 0;
+    for(int i = 0; i < b; i++){
+        if(vertex[i].color == WHITE) DFS_visit(vertex, i, finish_order, &fi);
     }
 }
 int main(){
     int b, a, mode, u, v;
     scanf("%d%d%d", &b, &a, &mode);
-    if(mode == 2) return 0;
-    Vertex vertex[b], trans_v[b]; 
-    Time finishOrder[b];//sorted by finish time from large to small 
-    for(int i = 0; i < b; i++){
-        vertex[i].color = WHITE;
-        vertex[i].parent = -1;
-        vertex[i].edgeHead = vertex[i].edgeTail = NULL;
-        trans_v[i].color = WHITE;
-        trans_v[i].parent = -1;
-        trans_v[i].edgeHead = trans_v[i].edgeTail = NULL;
-        finishOrder[i].idx = i, finishOrder[i].f = &(vertex[i].f);
-    }
+    Vertex vertex[B] = {0}, trans_v[B] = {0};
+    int finish_order[b];
     for(int i = 0; i < a; i++){
         scanf("%d%d", &u, &v);
         u--, v--;
         insertEdge(&vertex[u], v);
         insertEdge(&trans_v[v], u);
     }
-    DFS(vertex, b);
     #ifdef debug
     printEdge(vertex, b);
-    printTime(finishOrder, b);
-    #endif
-    qsort(finishOrder, b, sizeof(Time), cmp);
-    #ifdef debug
-    printTime(finishOrder, b);
-    #endif
-    int time = 0;
-    for(int i = 0; i < b; i++){
-        int idx = finishOrder[i].idx;
-        if(trans_v[idx].color == WHITE) DFS_visit(trans_v, &time, idx);
-    }
-    #ifdef debug
     printEdge(trans_v, b);
     #endif
-    int cnt = 0;
-    for(int i = 0; i < b; i++){
-        getParent(trans_v, i);
-        cnt += (trans_v[i].parent == -1);
+    DFS(vertex, b, finish_order);//1st dfs
+    
+    RootNodes *root_nodes = genRootNodes();
+    for(int i = 0; i < b; i++){//2nd dfs
+        int idx = finish_order[b - 1 - i];
+        #ifdef debug
+        printf("finish_order[%d] = %d\n", b - 1 - i, idx);
+        #endif
+        if(trans_v[idx].color == WHITE){
+            rnodes_push_back(root_nodes, idx);
+            dfs2(trans_v, idx, &(root_nodes->roots[ root_nodes->size - 1 ]));
+            trans_v[idx].parent = -1;
+            #ifdef debug
+            printf("trans_v[%d].color == WHITE, child:", idx);
+            RootNode rnode = root_nodes->roots[ root_nodes->size - 1 ];
+            for(int j = 0; j < rnode.child_cnt; j++){
+                printf(" %d", rnode.children[j]);
+            }
+            printf("\n");
+            #endif
+        }
     }
-    printf("%d\n", cnt);
+    printf("%d\n", root_nodes->size);
     return 0;
 }
