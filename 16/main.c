0d56b53e7b8b5ea97deb549e2491c0d390c54e9e
nat <nataliepjlin@gmail.com>
2024-04-25 15:43:25 +0800

    modify calculation of plag

--- 16/main.c
+++ 16/main.c
@@ -3,32 +3,30 @@
 #define LEN 5000001
 char text[LEN];//s
 char pattern[LEN];//c
-void createLPS(const int len, long long *lps){
-    lps[0] = 0;
-    int prev = 0, cur = 1;
+int createLPS(const int len, long long *lps, long long *plag){
+    lps[0] = 0, plag[0] = 1;
+    int prev = 0, cur = 1, cnt = 0;
     while(cur < len){
         if(pattern[prev] == pattern[cur]){
             lps[cur] = prev + 1;
+            plag[cur] = plag[ lps[cur] - 1 ] + 1;
             prev++, cur++;
         }
         else{
             if(prev != 0) prev = lps[prev - 1];
             else{
                 lps[cur] = 0;
+                plag[cur] = 1;
                 cur++;
             }
         }
     }
-    #ifdef debug
-    for(int i = 0; i < len ; i++)
-        printf("%lld%c", lps[i], " \n"[i == len - 1]);
-    #endif
 }
 int main(){
     scanf("%s%s", text, pattern);
     const int n = strlen(text), m = strlen(pattern);
-    long long lps[m], hits = 0;
-    createLPS(m, lps);
+    long long lps[m], plag[m], hits = 0;
+    createLPS(m, lps, plag);
     int idx[n], cnt = 0;
     for(int i = 0; i < n; i++){
         if(text[i] == pattern[0]){
@@ -36,7 +34,10 @@ int main(){
             hits++;
         }
     }
-    printf("%lld\n", hits * (lps[0] + 1));
+    #ifdef debug
+    printf("hits = %lld, plag = %lld, hits * plag = ", hits, plag[0]);
+    #endif
+    printf("%lld\n", hits * plag[0]);
     for(int j = 1; j < m; j++){
         int newcnt = 0; hits = 0;
         for(int c = 0; c < cnt; c++){
@@ -46,7 +47,10 @@ int main(){
             }
         }
         cnt = newcnt;
-        printf("%lld\n", hits * (lps[j] + 1));
+        #ifdef debug
+        printf("hits = %lld, plag = %lld, hits * plag = ", hits, plag[j]);
+        #endif
+        printf("%lld\n", hits * plag[j]);
     }
     return 0;
 }
