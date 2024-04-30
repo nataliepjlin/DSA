4f6df4dce54660c58cfbab8f39bd0e7dcba3ef91
nat <nataliepjlin@gmail.com>
2024-04-30 17:12:23 +0800

    test

--- 15/main.c
+++ 15/main.c
@@ -11,46 +11,23 @@ typedef struct Vertex{
     int *idx;
     int edge_size;
 }Vertex;
-typedef struct RootNode{
-    int id, visit;
-    int *children, child_cnt;
-    int *colors, color_cnt;
-}RootNode;
 typedef struct RootNodes{
-    RootNode *roots;
+    int *roots;
     int size;
 }RootNodes;
-RootNodes *gen_rnodes(){
-    RootNodes *rnodes = malloc(sizeof(RootNodes));
-    rnodes->roots = NULL, rnodes->size = 0;
-    return rnodes;
-}
-void rnodes_push_rnode(RootNodes *rnodes){
+void rnodes_push_rnode(RootNodes *rnodes, const int idx){
     rnodes->size += 1;
-    rnodes->roots = realloc(rnodes->roots, rnodes->size * (sizeof(RootNode)));
-    rnodes->roots[rnodes->size - 1].id = rnodes->size - 1;
-    rnodes->roots[rnodes->size - 1].child_cnt = rnodes->roots[rnodes->size - 1].color_cnt = 0;
-    rnodes->roots[rnodes->size - 1].children = rnodes->roots[rnodes->size - 1].colors = NULL;
+    rnodes->roots = realloc(rnodes->roots, rnodes->size * (sizeof(int)));
+    rnodes->roots[rnodes->size - 1] = idx;
 }
-void rnode_push_child(RootNode **rnode, const int idx){
-    (*rnode)->child_cnt += 1;
-    (*rnode)->children = realloc((*rnode)->children, (*rnode)->child_cnt * (sizeof(int)));
-    (*rnode)->children[ (*rnode)->child_cnt - 1 ] = idx;
-}
-void rnode_push_color(RootNode *rnode, const int c){
-    rnode->color_cnt += 1;
-    rnode->colors = realloc(rnode->colors, rnode->color_cnt * (sizeof(int)));
-    rnode->colors[rnode->color_cnt - 1] = c;
-}
-void dfs2(Vertex *trans_v, const int u, RootNode *rnode){//add every node's root parent
+void dfs2(Vertex *trans_v, const int u, const int root){//add every node's root parent
     trans_v[u].color = GREY;
     for(int i = 0; i < trans_v[u].edge_size; i++){
         if(trans_v[ trans_v[u].idx[i] ].color == WHITE){
-            trans_v[ trans_v[u].idx[i] ].parent = rnode->id;
-            dfs2(trans_v, trans_v[u].idx[i], rnode);
+            trans_v[ trans_v[u].idx[i] ].parent = root;
+            dfs2(trans_v, trans_v[u].idx[i], root);
         }
     }
-    rnode_push_child(&(rnode), u);
     trans_v[u].color = BLACK;
 }
 void insertEdge(Vertex *u, const int v){
@@ -99,42 +76,10 @@ void DFS(Vertex *vertex, const int b, int *finish_order){
 }
 void destroy_rnodes(RootNodes *rnodes){
     if(rnodes->size == 0) return;
-    for(int i = 0; i < rnodes->size; i++){
-        if(rnodes->roots[i].colors) free(rnodes->roots[i].colors);
-    }
     free(rnodes->roots);
 }
 int cmp(const void *a, const void *b){
     return *(int*)a - *(int*)b;
-}
-void printColor(RootNode *rnode){
-    printf("%d:", rnode->id);
-    for(int i = 0; i < rnode->color_cnt; i++){
-        printf(" %d", rnode->colors[i]);
-    }
-    printf("\n");
-}
-void dfs_topo(RootNode *roots, const int u, int *finish_order, int *fi){
-    roots[u].visit = GREY;
-    for(int i = roots[u].color_cnt - 1; i >= 0; i--){
-        if(i < roots[u].color_cnt - 1 
-        && roots[u].colors[i] == roots[u].colors[i + 1]) continue;
-        if(roots[ roots[u].colors[i] ].visit == WHITE){
-            dfs_topo(roots, roots[u].colors[i], finish_order, fi);
-        }
-    }
-    roots[u].visit = BLACK;
-    finish_order[*fi] = u;
-    *fi += 1;
-}
-int topo(RootNodes *rnodes){
-    int finish_order[rnodes->size], fi = 0;
-    dfs_topo(rnodes->roots, rnodes->size - 1, finish_order, &fi);
-    if(fi != rnodes->size) return 0;
-    for(int i = rnodes->size - 1; i >= 0; i--){
-        if(finish_order[i] != i) return 0;
-    }
-    return 1;
 }
 int main(){
     int b, a, mode, u, v;
@@ -157,48 +102,37 @@ int main(){
     for(int i = 0; i < b; i++){//2nd dfs
         int idx = finish_order[b - 1 - i];
         if(trans_v[idx].color == WHITE){
-            rnodes_push_rnode(&rnodes);
+            rnodes_push_rnode(&rnodes, idx);
             trans_v[idx].parent = rnodes.size - 1;
-            rnodes.roots[rnodes.size - 1].visit = WHITE;
-            dfs2(trans_v, idx, &(rnodes.roots[rnodes.size - 1]));
-            #ifdef debug
-            printf("SCC #%d:", rnodes.size - 1);
-            for(int j = 0; j < rnodes.roots[rnodes.size - 1].child_cnt; j++){
-                printf(" %d", rnodes.roots[rnodes.size - 1].children[j]);
-            }
-            printf("\n");
-            #endif
+            dfs2(trans_v, idx, rnodes.size - 1);
         }
     }
     printf("%d\n", rnodes.size);
     if(mode == 1) return 0; 
-    for(int i = 0; i < rnodes.size; i++){
-        for(int j = 0; j < rnodes.roots[i].child_cnt; j++){
-            const int cid = rnodes.roots[i].children[j];
-            for(int k = 0; k < trans_v[cid].edge_size; k++){
-                if(trans_v[ trans_v[cid].idx[k] ].parent != i){
-                    rnode_push_color(&(rnodes.roots[i]), trans_v[ trans_v[cid].idx[k] ].parent);
-                }
-            }
-            if(trans_v[cid].idx != NULL){//clean trans_v's edges
-                free(trans_v[cid].idx);
-                trans_v[cid].idx = NULL;
-                trans_v[cid].edge_size = 0;
-            }
-        }
-        if(rnodes.roots[i].children != NULL){//clean roots's children
-            free(rnodes.roots[i].children);
-            rnodes.roots[i].children = NULL;
-            rnodes.roots[i].child_cnt = 0;
-        }
-        if(rnodes.roots[i].color_cnt != 0){
-            qsort(rnodes.roots[i].colors, rnodes.roots[i].color_cnt, sizeof(int), cmp);
-        }
+    for(int i = 0; i < b; i++){
         #ifdef debug
-        printColor(&rnodes.roots[i]);
+        printf("trans_v[%d], parent = %d, edge = %d\n", i, trans_v[i].parent, trans_v[i].edge_size);
         #endif
+        for(int j = 0; j < trans_v[i].edge_size
+        && rnodes.roots[ trans_v[i].parent ] != -1; j++){
+            #ifdef debug
+            printf("trans_v[%d].parent = %d\n", trans_v[i].idx[j], trans_v[ trans_v[i].idx[j] ].parent);
+            #endif
+            if(trans_v[ trans_v[i].idx[j] ].parent + 1 == trans_v[i].parent){
+                rnodes.roots[ trans_v[i].parent ] = -1;
+            }
+        }
+        if(trans_v[i].idx != NULL){
+            free(trans_v[i].idx);
+            trans_v[i].idx = NULL;
+            trans_v[i].edge_size = 0;
+        }
     }
-    printf("%d\n", topo(&rnodes));
+    int valid = 1;
+    for(int i = rnodes.size - 1; i > 0 && valid; i--){
+        valid = (rnodes.roots[i] == -1);
+    }
+    printf("%d\n", valid);
     destroy_rnodes(&rnodes);
     return 0;
 }
